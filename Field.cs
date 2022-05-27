a5f725c569e2552b3bc4afe16d2ba44f2eb89b29
Michal Nebeský <77640963+NebeskyMichal@users.noreply.github.com>
2022-05-27 09:10:14 +0200

    Somewhat upgraded AI idk

---
+++ Field.cs
@@ -0,0 +1,222 @@
+﻿namespace ZaverecnyProjekt
+{
+    public class Field
+    {
+        public static int minX = 1;
+        public static int maxX = 119;
+        public static int minY = 5;
+        public static int maxY = 20;
+        private int maxEnemy = 10;
+        private List<Enemy> currEnemy;
+        private Random rand;
+        private Player player;
+        private List<BorderPiece> borders;
+
+        public List<Enemy> CurrEnemy { get => currEnemy; set => currEnemy = value; }
+        public Random Rand { get => rand; set => rand = value; }
+        public Player Player { get => player; set => player = value; }
+        public List<BorderPiece> Borders { get => borders; set => borders = value; }
+        public int MaxEnemy { get => maxEnemy; set => maxEnemy = value; }
+
+        public Field(Player player)
+        {
+            Rand = new Random();
+            CurrEnemy = new List<Enemy>();
+            Borders = new List<BorderPiece>();
+            this.Player = player;
+        }
+        /// <summary>
+        /// Creates borders around map so that player cant move out of bounds
+        /// </summary>
+        public void SetBorders()
+        {
+            for (int i = 0; i < 120; i++)
+            {
+                Borders.Add(new BorderPiece(i, 4));
+                Borders.Add(new BorderPiece(i, 20));
+            }
+            for (int j = 5; j < 20; j++)
+            {
+                Borders.Add(new BorderPiece(0, j));
+                Borders.Add(new BorderPiece(119, j));
+            }
+        }
+
+        /// <summary>
+        /// Method chooses random position in certain range and writes Q(Enemy Indicator) at that position
+        /// </summary>
+        public void SpawnEnemy()
+        {
+            int rX = rand.Next(minX, maxX);
+            int rY = rand.Next(minY, maxY);
+            Console.SetCursorPosition(rX, rY);
+            CurrEnemy.Add(new Enemy(rX, rY));
+            Console.Write("Q");
+            Console.SetCursorPosition(Player.CurrX + 1, Player.CurrY);
+        }
+        /// <summary>
+        /// Method used for repeating SpawnEnemy() in seperate thread
+        /// </summary>
+        public void Spawner()
+        {
+            while (CurrEnemy.Count < MaxEnemy)
+            {
+                Thread t = new Thread(SpawnEnemy);
+                t.Start();
+                t.Join();
+            }
+        }
+        /// <summary>
+        /// Method for moving enemies towards player in console
+        /// </summary>
+        public void EnemyMovement(int plX, int plY)
+        {
+            int pX = plX;
+            int pY = plY;
+            for (int i = 0; i < CurrEnemy.Count; i++)
+            {
+                int eX = CurrEnemy[i].CurrX;
+                int eY = CurrEnemy[i].CurrY;
+
+                if (pX - eX > 0 && pY - eY == 0)
+                {
+                    CurrEnemy[i].EnemyActions("right");
+                    Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                    Console.Write(player);
+                }
+                else if (pX - eX == 0 && pY - eY > 0)
+                {
+                    CurrEnemy[i].EnemyActions("down");
+                    Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                    Console.Write(player);
+
+                }
+                else if (eX - pX > 0 && eY - pY == 0)
+                {
+                    CurrEnemy[i].EnemyActions("left");
+                    Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                    Console.Write(player);
+                }
+                else if (pX - eX == 0 && eY - pY > 0)
+                {
+                    CurrEnemy[i].EnemyActions("up");
+                    Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                    Console.Write(player);
+                }
+                else
+                {
+                    if(pX < 60 && eX >= 60)
+                    {
+                        if (pY < 10 && eY >= 10)
+                        {
+                            CurrEnemy[i].EnemyActions("up");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }else if(pY > 10 && eY < 10)
+                        {
+                            CurrEnemy[i].EnemyActions("down");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                    }
+                    else if(pX >= 60 && eX < 60)
+                    {
+                          if (pY < 10 && eY >= 10) 
+                        {
+                            CurrEnemy[i].EnemyActions("up");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                        else if (pY > 10 && eY < 10)
+                        {
+                            CurrEnemy[i].EnemyActions("down");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                    }
+                   if (pX <= 60 && pY <= 20)
+                    {
+                        if (eX != 1)
+                        {
+                            CurrEnemy[i].EnemyActions("left");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                        else if (eX != 1 && eY > 5)
+                        {
+                            CurrEnemy[i].EnemyActions("up");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                    }
+                    else if (pX <= 60 && pY >= 20)
+                    {
+                        if (eX != 1)
+                        {
+                            CurrEnemy[i].EnemyActions("left");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                        else
+                        {
+                            CurrEnemy[i].EnemyActions("down");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                    }
+                    else if (pX >= 60 && pY <= 20)
+                    {
+                        if (eX != 118)
+                        {
+                            CurrEnemy[i].EnemyActions("right");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                        else if (eX != 118 && eY > 5 && eY < 10)
+                        {
+                            CurrEnemy[i].EnemyActions("up");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                    }
+                    else if (pX >= 60 && pY >= 20)
+                    {
+                        if (eX != 119)
+                        {
+                            CurrEnemy[i].EnemyActions("right");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                        }
+                        else if (eX != 120 && eY < 25 &&eY > 10)
+                        {
+                            CurrEnemy[i].EnemyActions("down");
+                            Console.SetCursorPosition(Player.CurrX, Player.CurrY);
+                            Console.Write(player);
+                       }
+                    }
+                }
+                BombCheck(CurrEnemy[i]);
+                if (eX == pX && eY == pY)
+                {
+                    Player.Health-=1;
+                    CurrEnemy.Remove(CurrEnemy[i]);
+                }
+            }
+        }
+        /// <summary>
+        /// Method that checks for bomb placement, if enemy and bomb connects remove both
+        /// </summary>
+        /// <param name="e"></param>
+        public void BombCheck(Enemy e)
+        {
+            for (int j = 0; j < Player.PlacedBombs.Count; j++)
+            {
+                if (e.CurrX == Player.PlacedBombs[j].CurrX && e.CurrY == Player.PlacedBombs[j].CurrY)
+                {
+                    CurrEnemy.Remove(e);
+                    Player.PlacedBombs.Remove(Player.PlacedBombs[j]);
+                }
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,164 @@
+﻿namespace ZaverecnyProjekt
+{
+    public class Player
+    {
+        private static char icon = 'X';
+        private int health;
+        private int currX;
+        private int currY;
+        private int facing;
+        private int maxBombs;
+        private List<Bomb> placedBombs;
+        private Field field;
+        private bool canPlace;
+        private string name;
+
+        public Field Field { get => field; set => field = value; }
+        public int Health { get => health; set => health = value; }
+        public int CurrX { get => currX; set => currX = value; }
+        public int CurrY { get => currY; set => currY = value; }
+        public int Facing { get => facing; set => facing = value; }
+        public int MaxBombs { get => maxBombs; set => maxBombs = value; }
+        public List<Bomb> PlacedBombs { get => placedBombs; set => placedBombs = value; }
+        public bool CanPlace { get => canPlace; set => canPlace = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Player(string name)
+        {
+            Health = 3;
+            CurrX = 60;
+            CurrY = 10;
+            Facing = 3;
+            MaxBombs = 10;
+            CanPlace = true;
+            PlacedBombs = new List<Bomb>();
+            Field = new Field(this);
+            Name = name;
+        }
+        /// <summary>
+        /// Method used for moving player in console using arrow keys and calling method PlaceBomb() using spacebar
+        /// </summary>
+        /// <param name="key"></param>
+        public void PlayerActions(ConsoleKey key)
+        {
+            Console.SetCursorPosition(CurrX + 1, CurrY);
+            switch (key)
+            {
+                case ConsoleKey.RightArrow:
+                    Console.Write("\b \b");
+                    Console.SetCursorPosition(CurrX += 1, CurrY);
+                    if (!this.CheckNextChar())
+                    {
+                        Console.SetCursorPosition(CurrX -= 1, CurrY);
+                        CanPlace = false;
+                        Console.Write(this);
+                        break;
+                    }
+                    Console.Write(this);
+                    Facing = 3;
+                    CanPlace = true;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.Write("\b \b");
+                    Console.SetCursorPosition(CurrX -= 1, CurrY);
+                    if (!this.CheckNextChar())
+                    {
+                        Console.SetCursorPosition(CurrX += 1, CurrY);
+                        CanPlace = false;
+                        Console.Write(this);
+                        break;
+                    }
+                    Console.Write(this);
+                    Facing = 1;
+                    CanPlace = true;
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.Write("\b \b");
+                    Console.SetCursorPosition(CurrX, CurrY += 1);
+                    if (!this.CheckNextChar())
+                    {
+                        Console.SetCursorPosition(CurrX, CurrY -= 1);
+                        CanPlace = false;
+                        Console.Write(this);
+                        break;
+                    }
+                    Console.Write(this);
+                    Facing = 2;
+                    CanPlace = true;
+                    break;
+                case ConsoleKey.UpArrow:
+                    Console.Write("\b \b");
+                    Console.SetCursorPosition(CurrX, CurrY -= 1);
+                    if (!this.CheckNextChar())
+                    {
+                        Console.SetCursorPosition(CurrX, CurrY += 1);
+                        CanPlace = false;
+                        Console.Write(this);
+                        break;
+                    }
+                    Console.Write(this);
+                    Facing = 4;
+                    CanPlace = true;
+                    break;
+                case ConsoleKey.Spacebar:
+                    this.PlaceBomb();
+                    break;
+            }
+        }
+        /// <summary>
+        /// Method determinating if the next character is either bomb,enemy or border
+        /// </summary>
+        public bool CheckNextChar()
+        {
+            PlacedBombs.RemoveAll(x => x.CurrX == CurrX && x.CurrY == CurrY);
+            if (Field.Borders.Exists(x => x.CurrX == this.CurrX && x.CurrY == this.CurrY))
+            {
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// Method writing O(Bomb indicator) to console until maximum bomb count is reached
+        /// </summary>
+        public void PlaceBomb()
+        {
+            if (PlacedBombs.Count < MaxBombs && CanPlace)
+            {
+                if (Facing == 3)
+                {
+                    Console.SetCursorPosition(CurrX + 1, CurrY);
+                    Console.Write('O');
+                    PlacedBombs.Add(new Bomb(CurrX + 1, CurrY));
+                    Console.SetCursorPosition(CurrX + 1, CurrY);
+                }
+                else if (Facing == 2)
+                {
+                    Console.SetCursorPosition(CurrX, CurrY + 1);
+                    Console.Write('O');
+                    PlacedBombs.Add(new Bomb(CurrX, CurrY + 1));
+                    Console.SetCursorPosition(CurrX + 1, CurrY);
+                }
+                else if (Facing == 4)
+                {
+                    Console.SetCursorPosition(CurrX, CurrY - 1);
+                    Console.Write('O');
+                    PlacedBombs.Add(new Bomb(CurrX, CurrY - 1));
+                    Console.SetCursorPosition(CurrX + 1, CurrY);
+                }
+                else if (Facing == 1)
+                {
+                    Console.SetCursorPosition(CurrX - 1, CurrY);
+                    Console.Write('O');
+                    PlacedBombs.Add(new Bomb(CurrX - 1, CurrY));
+                    Console.SetCursorPosition(CurrX + 1, CurrY);
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return icon.ToString();
+        }
+    }
+}
